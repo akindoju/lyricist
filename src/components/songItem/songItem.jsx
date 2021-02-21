5545f8040e7a70318903e3589e334111d21d0401
Akndju <davidd.akindoju@gmail.com>
2021-02-21 13:29:21 +0100

    added react hooks

--- src/components/songItem/songItem.jsx
+++ src/components/songItem/songItem.jsx
@@ -1,51 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import SongLyrics from '../songLyrics/songLyrics';
 import './songItem.css';
 
-class SongItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { lyrics: { lyrics: '' } };
-  }
+const SongItem = (props) => {
+  const [lyricsObj, setLyricsObj] = useState({ lyrics: '' });
+  const { song } = props;
 
-  getLyrics = () => {
+  const getLyrics = () => {
     console.log('clicked');
-    const { song } = this.props;
     fetch(`https://api.lyrics.ovh/v1/${song.artist.name}/${song.title}`)
       .then((res) => res.json())
-      .then((lyrics) => {
-        console.log(lyrics);
-        this.setState({ lyrics: lyrics });
+      .then((songLyrics) => {
+        setLyricsObj(songLyrics);
+        console.log(songLyrics);
       })
       .catch((error) => console.log(error));
   };
 
-  render() {
-    const { song } = this.props;
-    const lyrics = this.state.lyrics.lyrics;
-
-    return (
-      <li>
-        <div className="container">
-          <span>
-            <strong>{song.artist.name}</strong> - {song.title}
-          </span>
-          <button className="btn" onClick={this.getLyrics}>
-            Get Lyrics
-          </button>
-        </div>
-        <div>
-          {lyrics.split('\n').map((lyrics) => {
-            return (
-              <div className="lyrics">
-                {lyrics}
-                <br />
-              </div>
-            );
-          })}
-        </div>
-      </li>
-    );
-  }
-}
+  return (
+    <li>
+      <div className="container">
+        <span>
+          <strong>{song.artist.name}</strong> - {song.title}
+        </span>
+        <button className="btn" onClick={getLyrics}>
+          Get Lyrics
+        </button>
+      </div>
+      <SongLyrics lyrics={lyricsObj.lyrics} />
+    </li>
+  );
+};
 
 export default SongItem;
--- src/components/songLyrics/songLyrics.jsx
+++ src/components/songLyrics/songLyrics.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './songLyrics.css';
 
-const SongLyrics = (props) => {
-  const { song } = props;
-  return (
-    <div className="lyricsContainer">
-      <h2>
-        <strong>{song.artist.name}</strong> - {song.title}
-      </h2>
-      {/* <span>${lyrics}</span> */}
-    </div>
-  );
-};
+class SongLyrics extends Component {
+  render() {
+    const { lyrics } = this.props;
+
+    return (
+      <div>
+        {lyrics.split('\n').map((lyrics) => {
+          return (
+            <div className="lyrics">
+              {lyrics}
+              <br />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
 
 export default SongLyrics;
